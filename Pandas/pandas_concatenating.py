37914a670247ba870c34318a79cfe414b5248517
hbsjzgj <hbsjzgj@hotmail.com>
2017-07-08 11:34:35 +0900

    Create

--- Pandas/pandas_concatenating.py
+++ Pandas/pandas_concatenating.py
@@ -3,3 +3,139 @@ import numpy  as np
 
 
 #concatenating
+df1 = pd.DataFrame(np.ones((3,4))*0,columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1,columns=['a','b','c','d'])
+df3 = pd.DataFrame(np.ones((3,4))*2,columns=['a','b','c','d'])
+'''
+     a    b    c    d
+0  0.0  0.0  0.0  0.0
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+     a    b    c    d
+0  1.0  1.0  1.0  1.0
+1  1.0  1.0  1.0  1.0
+2  1.0  1.0  1.0  1.0
+     a    b    c    d
+0  2.0  2.0  2.0  2.0
+1  2.0  2.0  2.0  2.0
+2  2.0  2.0  2.0  2.0
+'''
+res = pd.concat([df1,df2,df3],axis = 0)
+'''res = pd.concat([df1,df2,df3],axis = 0,)
+     a    b    c    d
+0  0.0  0.0  0.0  0.0
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+0  1.0  1.0  1.0  1.0
+1  1.0  1.0  1.0  1.0
+2  1.0  1.0  1.0  1.0
+0  2.0  2.0  2.0  2.0
+1  2.0  2.0  2.0  2.0
+2  2.0  2.0  2.0  2.0
+'''
+
+res = pd.concat([df1,df2,df3],axis = 0,ignore_index= True)
+'''
+     a    b    c    d
+0  0.0  0.0  0.0  0.0
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+3  1.0  1.0  1.0  1.0
+4  1.0  1.0  1.0  1.0
+5  1.0  1.0  1.0  1.0
+6  2.0  2.0  2.0  2.0
+7  2.0  2.0  2.0  2.0
+8  2.0  2.0  2.0  2.0
+'''
+
+#join,['inner','outer']
+df4 = pd.DataFrame(np.ones((3,4))*0,columns=['a','b','c','d'],index=[1,2,3])
+df5 = pd.DataFrame(np.ones((3,4))*1,columns=['b','c','d','e'],index=[2,3,4])
+'''
+     a    b    c    d
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+3  0.0  0.0  0.0  0.0
+     b    c    d    e
+2  1.0  1.0  1.0  1.0
+3  1.0  1.0  1.0  1.0
+4  1.0  1.0  1.0  1.0
+'''
+res = pd.concat([df4,df5],join = 'outer')
+''' 两个数列 合并 
+     a    b    c    d    e
+1  0.0  0.0  0.0  0.0  NaN
+2  0.0  0.0  0.0  0.0  NaN
+3  0.0  0.0  0.0  0.0  NaN
+2  NaN  1.0  1.0  1.0  1.0
+3  NaN  1.0  1.0  1.0  1.0
+4  NaN  1.0  1.0  1.0  1.0
+'''
+res = pd.concat([df4,df5],join = 'inner')
+''' 两个数列 合并
+     b    c    d
+1  0.0  0.0  0.0
+2  0.0  0.0  0.0
+3  0.0  0.0  0.0
+2  1.0  1.0  1.0
+3  1.0  1.0  1.0
+4  1.0  1.0  1.0
+'''
+res = pd.concat([df4,df5],join = 'inner',ignore_index=True)
+'''
+     b    c    d
+0  0.0  0.0  0.0
+1  0.0  0.0  0.0
+2  0.0  0.0  0.0
+3  1.0  1.0  1.0
+4  1.0  1.0  1.0
+5  1.0  1.0  1.0
+'''
+
+#join_axes
+res = pd.concat([df4,df5],axis = 1,join_axes=[df4.index])
+'''
+     a    b    c    d    b    c    d    e
+1  0.0  0.0  0.0  0.0  NaN  NaN  NaN  NaN
+2  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+3  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+'''
+
+#append
+res = df4.append(df5,ignore_index=True)
+'''
+     a    b    c    d    e
+0  0.0  0.0  0.0  0.0  NaN
+1  0.0  0.0  0.0  0.0  NaN
+2  0.0  0.0  0.0  0.0  NaN
+3  NaN  1.0  1.0  1.0  1.0
+4  NaN  1.0  1.0  1.0  1.0
+5  NaN  1.0  1.0  1.0  1.0
+'''
+df6 = pd.DataFrame(np.ones((3,4))*1,columns=['b','c','d','e'])
+
+res = df4.append([df5,df6])
+'''
+     a    b    c    d    e
+1  0.0  0.0  0.0  0.0  NaN
+2  0.0  0.0  0.0  0.0  NaN
+3  0.0  0.0  0.0  0.0  NaN
+2  NaN  1.0  1.0  1.0  1.0
+3  NaN  1.0  1.0  1.0  1.0
+4  NaN  1.0  1.0  1.0  1.0
+0  NaN  1.0  1.0  1.0  1.0
+1  NaN  1.0  1.0  1.0  1.0
+2  NaN  1.0  1.0  1.0  1.0
+'''
+
+s1 = pd.Series([1,2,3,4],index=['a','b','c','d'])
+res = df4.append(s1,ignore_index=True)
+'''
+     a    b    c    d
+0  0.0  0.0  0.0  0.0
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+3  1.0  2.0  3.0  4.0
+'''
+print(res)
+
